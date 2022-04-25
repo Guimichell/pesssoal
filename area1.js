2d3493721f6756cbe97c394fb84db8167abe5cb3
Guimichell <88491494+Guimichell@users.noreply.github.com>
2022-04-25 16:06:45 -0300

    Add files via upload

---
+++ area1.js
@@ -0,0 +1,182 @@
+var a = window.document.getElementById('area1')
+a.addEventListener('click',clicar)
+a.addEventListener('mouseenter',entrou)
+a.addEventListener('mouseout' ,saiu)
+
+function clicar() {
+    a.innerText = 'Clicou!'
+    a.style.background = 'black'
+}
+function entrou() {
+    a.innerText = 'Entrou!'
+    a.style.background = 'green'
+}
+function saiu() {
+    a.innerText = 'Saiu!'
+    a.style.background = 'red'
+}
+
+var b = window.document.getElementById('area2')
+b.addEventListener('click', clicar2)
+b.addEventListener('mouseenter', entrou2)
+b.addEventListener('mouseout', saiu2)
+
+function clicar2() {
+    b.innerText = 'Clicou'
+    b.style.background = '#b29a78'
+}
+function entrou2() {
+    b.innerText = 'Entrou'
+    b.style.background = '#f07f83'
+}
+function saiu2() {
+    b.innerText = 'saiu'
+    b.style.background = '#907071'
+}
+
+var c = window.document.getElementById('area3')
+c.addEventListener('click', clicar3)
+c.addEventListener('mouseenter', entrou3)
+c.addEventListener('mouseout', saiu3)
+
+function clicar3() {
+    c.innerText = 'Clicou'
+    c.style.background = '#af162a'
+}
+function entrou3() {
+    c.innerText = 'Entrou'
+    c.style.background = '#44021e'
+}
+function saiu3() {
+    c.innerText = 'saiu'
+    c.style.background = '#5a0e3d'
+}
+
+var d = window.document.getElementById('area4')
+d.addEventListener('click',clicar4)
+d.addEventListener('mouseenter',entrou4)
+d.addEventListener('mouseout' ,saiu4)
+
+function clicar4() {
+    d.innerText = 'Clicou!'
+    d.style.background = '#322030'
+}
+function entrou4() {
+    d.innerText = 'Entrou!'
+    d.style.background = '#725b75'
+}
+function saiu4() {
+    d.innerText = 'Saiu!'
+    d.style.background = '#c1a2a0'
+}
+
+var e = window.document.getElementById('area5')
+e.addEventListener('click',clicar5)
+e.addEventListener('mouseenter',entrou5)
+e.addEventListener('mouseout',saiu5)
+
+function clicar5() {
+    e.innerText = 'clicou'
+    e.style.background = '#ee2853'
+}
+function entrou5() {
+    e.innerText = 'entrou'
+    e.style.background = '#c3b457'
+}
+function saiu5() {
+    e.innerText = 'saiu'
+    e.style.background = '#5b1d99'
+}
+
+var f = window.document.getElementById('area6')
+f.addEventListener('click',clicar6)
+f.addEventListener('mouseenter',entrou6)
+f.addEventListener('mouseout' ,saiu6)
+
+function clicar6() {
+    f.innerText = 'Clicou!'
+    f.style.background = 'black'
+}
+function entrou6() {
+    f.innerText = 'Entrou!'
+    f.style.background = 'green'
+}
+function saiu6() {
+    f.innerText = 'Saiu!'
+    f.style.background = 'red'
+}
+
+var g = window.document.getElementById('area7')
+g.addEventListener('click', clicar7)
+g.addEventListener('mouseenter', entrou7)
+g.addEventListener('mouseout', saiu7)
+
+function clicar7() {
+    g.innerText = 'Clicou'
+    g.style.background = '#b29a78'
+}
+function entrou7() {
+    g.innerText = 'Entrou'
+    g.style.background = '#f07f83'
+}
+function saiu7() {
+    g.innerText = 'saiu'
+    g.style.background = '#907071'
+}
+
+var h = window.document.getElementById('area8')
+h.addEventListener('click', clicar8)
+h.addEventListener('mouseenter', entrou8)
+h.addEventListener('mouseout', saiu8)
+
+function clicar8() {
+    h.innerText = 'Clicou'
+    h.style.background = '#af162a'
+}
+function entrou8() {
+    h.innerText = 'Entrou'
+    h.style.background = '#44021e'
+}
+function saiu8() {
+    h.innerText = 'saiu'
+    h.style.background = '#5a0e3d'
+}
+
+var i = window.document.getElementById('area9')
+i.addEventListener('click',clicar9)
+i.addEventListener('mouseenter',entrou9)
+i.addEventListener('mouseout' ,saiu9)
+
+function clicar9() {
+    i.innerText = 'Clicou!'
+    i.style.background = '#322030'
+}
+function entrou9() {
+    i.innerText = 'Entrou!'
+    i.style.background = '#725b75'
+}
+function saiu9() {
+    i.innerText = 'Saiu!'
+    i.style.background = '#c1a2a0'
+}
+
+var j = window.document.getElementById('area10')
+j.addEventListener('click',clicar10)
+j.addEventListener('mouseenter',entrou10)
+j.addEventListener('mouseout',saiu10)
+
+function clicar10() {
+    j.innerText = 'clicou'
+    j.style.background = '#ee2853'
+}
+function entrou10() {
+    j.innerText = 'entrou'
+    j.style.background = '#c3b457'
+}
+function saiu10() {
+    j.innerText = 'saiu'
+    j.style.background = '#5b1d99'
+}
+
+
+
